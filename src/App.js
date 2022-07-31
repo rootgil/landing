1e4548346e645cd509ccdf3382bd69860a2678b8
Gilles AHOUANTCHEDE <gilles@gilles.root@geek>
2022-07-31 18:32:19 +0200

    final job

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import './App.css';
+import Home from './components/pages/Home';
+import Services from './components/pages/Services';
+import Products from './components/pages/Products';
+import SignUp from './components/pages/SignUp';
+import Footer from './components/Footer'
+
+function App() {
+    return (
+            <Router>
+                <Navbar />
+                <Routes>
+                    <Route path='/' exact element={<Home />} />
+                    <Route path='/services' exact element={<Services/>} />
+                    <Route path='/products' exact element={<Products />} />
+                    <Route path='/sign-up' exact element={<SignUp/>} />
+                </Routes>
+                <Footer />
+            </Router>
+    );
+}
+
+export default App;
